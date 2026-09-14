7986b105e87c920007c5c4b164cad7d88c48d396
Dhairya Anchal <dharya.anchal@gmail.com>
2026-09-14 13:44:53 +0530

    init: font loaders

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import { sum } from "./sum";
+import "./style/style.scss";
+import "./fonts/OpenSans-Italic-VariableFont_wdth,wght.ttf";
 
 document.getElementById("btn").addEventListener("click", () => {
 	const result = sum(2, 3);
